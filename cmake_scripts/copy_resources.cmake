f985b8752d12c71acd9c739351ffe814cc6e7703
Saviz Mohammadi <savizmohammadi1379@gmail.com>
2024-12-06 11:49:05 -0800

    Improved resource system to show resources in IDE

--- cmake_scripts/copy_resources.cmake
+++ cmake_scripts/copy_resources.cmake
@@ -1,34 +1,118 @@
-# [[ Custom Targets - CopyResources ]]
+# [[ Custom target for detecting resources ]]
 # [[ ----------------------------------------------------------------------- ]]
 # [[ ----------------------------------------------------------------------- ]]
 
-# Custom target for copying resource files.
-add_custom_target(CopyResources)
+# This script section is useful for displaying the files in IDEs.
+
+file(GLOB_RECURSE
+        
+        TXT_FILES
+        
+        "${PROJECT_SOURCE_DIR}/resources/txt/*")
+
+file(GLOB_RECURSE
+        
+        QML_FILES
+        
+        "${PROJECT_SOURCE_DIR}/resources/qml/*")
+        
+file(GLOB_RECURSE
+        
+        MUSIC_FILES
+        
+        "${PROJECT_SOURCE_DIR}/resources/music/*")
+        
+file(GLOB_RECURSE
+        
+        JSON_FILES
+        
+        "${PROJECT_SOURCE_DIR}/resources/json/*")
+        
+file(GLOB_RECURSE
+        
+        ICON_FILES
+        
+        "${PROJECT_SOURCE_DIR}/resources/icons/*")
+
+file(GLOB_RECURSE
+        
+        FONT_FILES
+        
+        "${PROJECT_SOURCE_DIR}/resources/fonts/*")
 
 
 
-foreach(RESOURCE_FILE ${RESOURCE_FILES})
+source_group("Resources\\TXT"
+        
+        FILES
+        
+        ${TXT_FILES})
+        
+source_group("Resources\\QML"
+        
+        FILES
+        
+        ${QML_FILES})
 
-    # Get the relative path of the current resource file from where it differs to ${RESOURCE_BASE_ORIGIN_LOCATION}.
-    file(RELATIVE_PATH REL_PATH ${RESOURCE_BASE_ORIGIN_LOCATION} ${RESOURCE_FILE})
+source_group("Resources\\MUSIC"
+        
+        FILES
+        
+        ${MUSIC_FILES})
+        
+source_group("Resources\\JSON"
+        
+        FILES
+        
+        ${JSON_FILES})
 
-    # Construct the destination path including relative subdirectory.
-    set(DESTINATION_PATH "${RESOURCE_BASE_DESTINATION_LOCATION}/${REL_PATH}")
+source_group("Resources\\ICONS"
+        
+        FILES
+        
+        ${ICONS_FILES})
+        
+source_group("Resources\\FONTS"
+        
+        FILES
+        
+        ${FONTS_FILES})
 
-    message(STATUS "Copying ${RESOURCE_FILE} to ${DESTINATION_PATH}")
-
-    # Custom command to copy the resource file.
-    add_custom_command(TARGET CopyResources POST_BUILD
-        COMMAND ${CMAKE_COMMAND} -E copy_if_different
-        "${RESOURCE_FILE}"
-        "${DESTINATION_PATH}"
-        COMMENT "Copying ${RESOURCE_FILE} to ${DESTINATION_PATH}")
-endforeach()
 
 
+add_custom_target(DetectResources
+        
+        SOURCES
+
+        ${TXT_FILES}
+        ${QML_FILES}
+        ${MUSIC_FILES}
+        ${JSON_FILES}
+        ${ICON_FILES}
+        ${FONT_FILES})
+
+add_dependencies(${EXECUTABLE_NAME} DetectResources)
+
+# [[ ----------------------------------------------------------------------- ]]
+# [[ ----------------------------------------------------------------------- ]]
+# [[ Custom target for detecting resources ]]
+
+
+
+
+
+# [[ Custom target for copying resources ]]
+# [[ ----------------------------------------------------------------------- ]]
+# [[ ----------------------------------------------------------------------- ]]
+
+add_custom_target(CopyResources ALL
+        COMMAND ${CMAKE_COMMAND} -E copy_directory
+        ${PROJECT_SOURCE_DIR}/resources
+        ${CMAKE_RUNTIME_OUTPUT_DIRECTORY}/resources
+        COMMENT "Copying resources into executable location")
 
 add_dependencies(${EXECUTABLE_NAME} CopyResources)
 
 # [[ ----------------------------------------------------------------------- ]]
 # [[ ----------------------------------------------------------------------- ]]
-# [[ Custom Targets - CopyResources ]]
+# [[ Custom target for copying resources ]]
